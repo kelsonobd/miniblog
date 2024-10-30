8f319ee36dce98fa1992d0ea0e81d434ede3be06
Kelsonobd <kelsonobd@gmail.com>
2024-10-30 17:00:58 -0300

    autenticacao do input senha parcialmente concluido

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,29 @@ import "./App.css";
 
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
+//components
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+//pages
+import Home from "./pages/Home/Home";
+import About from "./pages/About/About";
+import Login from "./pages/Register/Register";
+import Register from "./pages/Register/Register";
+
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <Navbar />
         <div className="container">
-          <Routes></Routes>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+          </Routes>
         </div>
+        <Footer />
       </BrowserRouter>
     </div>
   );
---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import styles from "./Footer.module.css";
+
+const Footer = () => {
+  return (
+    <footer className={styles.footer}>
+      <h3>Frase que você deseja colocar!</h3>
+      <p>Mini blog &copy; 2024</p>
+    </footer>
+  );
+};
+
+export default Footer;
